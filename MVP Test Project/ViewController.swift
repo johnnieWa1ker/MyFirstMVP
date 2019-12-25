cc3759f11010577ed0f8fe8346495bf337172b60
Johnnie Walker <rockwave.fm@gmail.com>
2019-12-25 18:58:49 +0300

    Initial Commit

---
+++ MVP Test Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVP Test Project
+//
+//  Created by Johnnie Walker on 25.12.2019.
+//  Copyright © 2019 Johnnie Walker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
